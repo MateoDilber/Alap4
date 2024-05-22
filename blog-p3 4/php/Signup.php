953b85df9181d10c5aeee1dedad5c1fa9a68d04c
MateoDilber <6035857@mborijnland.nl>
2024-05-22 11:18:15 +0200

    ..

--- blog-p3 4/php/Signup.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include 'dbconnect.php';
-  
-$username = $_POST['gebruikersnaam'];
-$email = $_POST['email'];
-$password = $_POST['wachtwoord'];
-insertData($username, $email, $password);
-?>
